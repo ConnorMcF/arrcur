5444604bf25bd7824fba4dc9d901dbb858b75c97
xmatmen <gcmatmen@gmail.com>
2017-02-11 23:36:27 +0100

    Update app.js
    Removed unnecessary counter
    Using strict checking

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
 module.exports = function(arr, cb, done) {
-	var arrcur = 0;
 	if(arr.constructor === Array) {
 		var arrlen = arr.length - 1;
 		if(arr.length == 0) { done(); }
-		arr.forEach(function(val, key) {
+		arr.forEach(function(val, key, index) {
 			cb(val, key);
-			if(arrcur == arrlen) {
+			if(index === arrlen) {
 				done();
 			}
-			arrcur++;
 		});
 	} else if(arr.constructor === Object) {
 		var arrlen = Object.keys(arr).length - 1;
 		if(arr.length == 0) { done(); }
-		Object.keys(arr).forEach(function(key) {
+		Object.keys(arr).forEach(function(key, index) {
 			cb(arr[key], key);
-			if(arrcur == arrlen) {
+			if(index === arrlen) {
 				done();
 			}
-			arrcur++;
 		});
 	} else if(arr.constructor === String) {
 		var arrlen = arr.split('').length;
 		if(arr.length == 0) { done(); }
-		arr.split('').forEach(function(val, key) {
+		arr.split('').forEach(function(val, key, index) {
 			cb(val, key);
-			if(arrcur == arrlen) {
+			if(index === arrlen) {
 				done();
 			}
-			arrcur++;
 		});
 	} else {
 		throw new TypeError("Unable to iterate " + arr.constructor);
